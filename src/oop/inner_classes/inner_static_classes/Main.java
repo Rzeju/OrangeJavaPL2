6663c70b270593abaabe1b3e7fd2cd0938a90174
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-12-17 12:10:02 +0100

    inner static class - comments

--- src/oop/inner_classes/inner_static_classes/Main.java
+++ src/oop/inner_classes/inner_static_classes/Main.java
@@ -4,11 +4,18 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Utworzenie obiekty statycznej klasy wewnętrznej
+        //Nie potrzeba do tego utowrzenia obiektu klasy zewnętrznej
         MyOuterClass.MyStaticInnerClass myStaticInnerClass_1 = new MyOuterClass.MyStaticInnerClass();
 
+        //Utowrzenie obiektu klasy zewnętrznej
         MyOuterClass myOuterClass = new MyOuterClass();
+        //Utworzenie obiektu klasy wewnętrznej metodą obiektu klasy zewnętrznej
+        //metoda init() nie jest statyczna dlatego wymaga obiektu klasy zewnętrznej
         MyOuterClass.MyStaticInnerClass myStaticInnerClass_2 = myOuterClass.init();
 
+        //Utworzenie obiektu klasy wewnętrznej metodą statyczną klasy zewnętrznej
+        //Metoda statyczna nie wymaga utworzenia obiektu klasy zewnętrznej
         MyOuterClass.MyStaticInnerClass myStaticInnerClass_3 = MyOuterClass.staticInit();
 
     }
--- src/oop/inner_classes/inner_static_classes/MyOuterClass.java
+++ src/oop/inner_classes/inner_static_classes/MyOuterClass.java
@@ -1,15 +1,19 @@
 package oop.inner_classes.inner_static_classes;
 
+//Klasa zewnętrzna
 public class MyOuterClass {
 
+    //Statyczna klasa wewnętrzna
     public static class MyStaticInnerClass {
 
     }
 
+    //metoda zwracająca obiekt statycznej klasy wewnętrznej
     public MyStaticInnerClass init() {
         return new MyStaticInnerClass();
     }
 
+    //statyczna metoda zwracająca obiekt statycznej klasy wewnętrznej
     public static MyStaticInnerClass staticInit() {
         return new MyStaticInnerClass();
     }
