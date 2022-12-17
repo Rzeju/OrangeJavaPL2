bf83a081738e19997d274fad66bb11f88e6241c8
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-12-17 10:20:43 +0100

    static example - more

--- src/oop/statics/Main.java
+++ src/oop/statics/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Użycie metody statycznej z naszej klasy
+        int result = StaticExample.add(5, 10);
+        System.out.println(result);
+
+        //Użycie stałych statycznych z klasy Math
+        double myPi = Math.PI;
+        double myE = Math.E;
+
+        //Przypisanie nowej wartości do pola statycznego w naszej klasie
+        StaticExample.myStaticField = 20;
+        System.out.println(StaticExample.myStaticField);
+
     }
 }
--- src/oop/statics/StaticExample.java
+++ src/oop/statics/StaticExample.java
@@ -2,6 +2,8 @@ package oop.statics;
 
 public class StaticExample {
 
+    public static int myStaticField = 10;
+
     public static int add(int a, int b) {
         return a + b;
     }
