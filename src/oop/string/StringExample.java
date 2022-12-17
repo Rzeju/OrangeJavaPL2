a4b406d2f5dcb6ae71f970bda5d86a28fff2b076
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-12-17 13:44:28 +0100

    String class exmaple

--- src/oop/string/StringExample.java
+++ src/oop/string/StringExample.java
@@ -16,8 +16,25 @@ public class StringExample {
         System.out.println("finalText_1");
         System.out.println(finalText_1);
 
+        //Przykład konkatenacji Stringów metodą concat (wynik jest taki sam jak w sposobie powyżej)
         String finalText_2 = myString.concat("\n").concat(myStringObject);
         System.out.println("finalText_2");
         System.out.println(finalText_2);
+
+        System.out.println("Porównanie finalText_1 i finalText_2 = " + finalText_1.equals(finalText_2));
+        System.out.println("Porównanie myString i myStringObject = " + myString.equals(myStringObject));
+
+        myString.intern();
+
+        System.out.println("Długość myString = " + myString.length());
+        System.out.println("myString upperCase() = " + myString.toUpperCase());
+        System.out.println("myString isBlank() = " + myString.isBlank());
+        System.out.println("myString is Empty() = " + myString.isEmpty());
+        System.out.println("Metoda isEmpty()");
+        System.out.println("".isEmpty());
+        System.out.println("     ".isEmpty());
+        System.out.println("Metoda isBlank()");
+        System.out.println("".isBlank());
+        System.out.println("     ".isBlank());
     }
 }
