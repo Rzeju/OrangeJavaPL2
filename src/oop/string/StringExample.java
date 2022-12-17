20c4ebc555f4b99b1a45ce28fe7630b7e87ba7c5
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-12-17 13:59:27 +0100

    String class exmaple

--- src/oop/string/StringExample.java
+++ src/oop/string/StringExample.java
@@ -21,11 +21,14 @@ public class StringExample {
         System.out.println("finalText_2");
         System.out.println(finalText_2);
 
+        //Przykład metody equals()
         System.out.println("Porównanie finalText_1 i finalText_2 = " + finalText_1.equals(finalText_2));
         System.out.println("Porównanie myString i myStringObject = " + myString.equals(myStringObject));
 
+        //Przykłąd metody intern() -> (warto pamiętać o String pool'u i przypomnieć sobie wiadomości o nim)
         myString.intern();
 
+        //Przykłady metod z klasy String
         System.out.println("myString" + myString);
         System.out.println("Długość myString = " + myString.length());
         System.out.println("myString upperCase() = " + myString.toUpperCase());
@@ -39,8 +42,21 @@ public class StringExample {
         System.out.println("     ".isBlank());
 
 
+        //Więcej metod z klasy String
         System.out.println("myStringObject = " + myStringObject);
         System.out.println("myStringObject indexOf('my') = " + myStringObject.indexOf("my"));
         System.out.println(myStringObject.substring(8));
+
+        //Przykład metody split oraz wyświetlenie elemnetów tablicy, którą utworzyła
+        String[] splittedString = myStringObject.split(" ");
+        for (int i = 0; i < splittedString.length; i++) {
+            System.out.println("Index = " + i + ", element = " + splittedString[i]);
+        }
+
+        //Przykład metody split otaz pętli foreach na tablicy, którą utowrzyła metoda
+        String[] myLetters = myStringObject.split("");
+        for(String elem : myLetters) {
+            System.out.println(elem);
+        }
     }
 }
