1a9287d0312be3849cae1cbb87789367e17f9dfa
Piotr Andrzejak <pandrzejak93@gmail.com>
2022-12-17 11:51:37 +0100

    inner class example - comments

--- src/oop/inner_classes/Main.java
+++ src/oop/inner_classes/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 
+        //Utworzenie klasy zewnętrznej
         MyOuterClass myOuterClass = new MyOuterClass();
 
+        //Utworzenie klasy wewnętrznej metodą z klasy zewnętrznej
         MyOuterClass.MyInnerClass myInnerClass_1 = myOuterClass.init();
+        //Utworzenie klasy wewnętrznej konstruktorem tej klasy przy użyciu obiektu klasy zewnętrznej
         MyOuterClass.MyInnerClass myInnerClass_2 = myOuterClass.new MyInnerClass();
 
+        //Użycie metod obiektów klasy wewnętrznej
         myInnerClass_1.printNumber();
         myInnerClass_2.printNumber();
     }
--- src/oop/inner_classes/MyOuterClass.java
+++ src/oop/inner_classes/MyOuterClass.java
@@ -1,14 +1,22 @@
 package oop.inner_classes;
 
+//Klasa zewnętrzna
 public class MyOuterClass {
 
+    //stała w klasie zewnętrznej
     private final int myConstance = 5;
 
+    //klasa wewnętrzna
     public class MyInnerClass {
+        //metoda w klasie wewnętrznej
+        //ma ona dostęp do pola klasy zewnętrznej
         public void printNumber() {
             System.out.println(myConstance);
         }
     }
+
+    //metoda klasy zewnętrznej
+    //tworzy obiekt klasy wewnętrznej i zwraca go
     public MyInnerClass init() {
         return new MyInnerClass();
     }
